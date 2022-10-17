b682c66270c9786c043830e4f35340ec1fd36a2a
Daniel Dietrich <cafebab3@gmail.com>
2022-10-17 11:04:15 +0000

    Update TypeLevel description Fix keyof T shortcomings Fix IsEmpty and Filter Move predicates, fix Paths Handle any and never special in Paths Add warning to README.md

--- src/utilities.ts
+++ src/utilities.ts
@@ -4,40 +4,6 @@
  * terms of the MIT License, which is available in the project root.
  ******************************************************************************/
 
-// returns true if T1 is exactly T2
-export type Is<T1, T2> =
-    (<T>() => T extends T2 ? true : false) extends (<T>() => T extends T1 ? true : false)
-        ? true
-        : false;
-export type Extends<A1, A2> =
-    Is<A1, never> extends true ? true :
-        Is<A2, never> extends true ? false :
-            Is<A2, any> extends true ? true :
-                Is<A2, unknown> extends true ? true :
-                    Is<A1, any> extends true ? false :
-                        Is<A1, unknown> extends true ? false :
-                            A1 extends A2 ? true : false;
-
-// logical and
-export type And<C1 extends boolean, C2 extends boolean> =
-    C1 extends true
-        ? C2 extends true
-            ? true
-            : false
-        : false;
-
-// logical or
-export type Or<C1 extends boolean, C2 extends boolean> =
-    C1 extends true
-        ? true
-        : C2 extends true
-            ? true
-            : false;
-
-// logical not
-export type Not<C extends boolean> =
-    C extends true ? false : true;
-
 // [A, B] => A & B
 export type TupleToIntersection<T extends any[]> =
     UnionToIntersection<TupleToUnion<T>>;
--- test/objects.test.ts
+++ test/objects.test.ts
@@ -4,9 +4,151 @@
  * terms of the MIT License, which is available in the project root.
  ******************************************************************************/
 
-import { assertType } from 'typelevel-assert';
-import { Fn } from 'tinybench';
-import { describe, it } from 'vitest'
-import { Is } from '../src';
+import { assertType } from "typelevel-assert";
+import { describe, it } from "vitest";
+import { Is, IsEmpty, Join, Keys, Paths, Values } from "../src";
 
-// TODO(@@dd)
+describe('IsEmpty<T>', () => {
+
+    it('should work for universal types', () => {
+        assertType<Is<IsEmpty<any>, true>>();
+        assertType<Is<IsEmpty<unknown>, true>>();
+        assertType<Is<IsEmpty<never>, true>>();
+        assertType<Is<IsEmpty<{}>, true>>();
+    });
+
+    it('should work non-empty object', () => {
+        type Actual = IsEmpty<{ a: 1 }>;
+        type Expected = false;
+        assertType<Is<Actual, Expected>>();
+    });
+
+});
+
+describe('Keys<T>', () => {
+
+    it('should fix keyof any = string | number | symbol', () => {
+        type Actual = Keys<any>;
+        type Expected = never
+        assertType<Is<keyof any, string | number | symbol>>();
+        assertType<Is<Actual, Expected>>();
+    });
+
+    it('should behave like keyof unknown = never', () => {
+        type Actual = Keys<unknown>;
+        type Expected = never;
+        assertType<Is<Actual, keyof unknown>>();
+        assertType<Is<Actual, Expected>>();
+    });
+
+    it('should fix keyof never = string | number | symbol', () => {
+        type Actual = Keys<never>;
+        type Expected = never
+        assertType<Is<keyof never, string | number | symbol>>();
+        assertType<Is<Actual, Expected>>();
+    });
+
+});
+
+describe('Values<T>', () => {
+
+    it('should behave like any[keyof any] = any', () => {
+        type Actual = Values<any>;
+        type Expected = any;
+        assertType<Is<Actual, any[keyof any]>>();
+        assertType<Is<Actual, Expected>>();
+    });
+
+    it('should behave like unknown[keyof unknown] = never', () => {
+        type Actual = Values<unknown>;
+        type Expected = never;
+        assertType<Is<Actual, unknown[keyof unknown]>>();
+        assertType<Is<Actual, Expected>>();
+
+    });
+
+    it('should behave like never[keyof never] = never', () => {
+        type Actual = Values<never>;
+        type Expected = never
+        assertType<Is<Actual, never[keyof never]>>();
+        assertType<Is<Actual, Expected>>();
+    });
+
+});
+
+describe('Join', () => {
+
+    it('should join universal types', () => {
+        assertType<Is<Join<any>, any>>();
+        assertType<Is<Join<unknown>, unknown>>();
+        assertType<Is<Join<never>, never>>();
+        assertType<Is<Join<{}>, {}>>();
+    });
+
+    it('should fulfil prerequisite Is<{ a: 1 } & { b: 2 }, { a: 1, b: 2 }> extends false', () => {
+        type Actual = { a: 1 } & { b: 2 };
+        type Expected = { a: 1, b: 2 };
+        // @ts-expect-error
+        assertType<Is<Actual, Expected>>();
+    });
+
+    it('should join intersection', () => {
+        type Actual = Join<{ a: 1 } & { b: 2 }>;
+        type Expected = { a: 1, b: 2 };
+        assertType<Is<Actual, Expected>>();
+    });
+
+});
+
+describe('Paths', () => {
+
+    it('should work for universal types T = any | unknown | never', () => {
+        assertType<Is<Paths<any>, any>>();
+        assertType<Is<Paths<never>, never>>();
+        assertType<Is<Paths<unknown>, unknown>>();
+    });
+
+    it('should work for type T = {}', () => {
+        type Actual = Paths<{}>;
+        type Expected = {};
+        assertType<Is<Actual, Expected>>();
+    });
+
+    it('should work if a property value is never', () => {
+        type Actual = Paths<{
+            b: never;
+        }>;
+        type Expected = {
+            b: never
+        };
+        assertType<Is<Actual, Expected>>();
+    });
+
+    it('should handle any', () => {
+        type T = {
+            _: any;
+        };
+        type Actual = Paths<T>;
+        type Expected = T;
+        assertType<Is<Actual, Expected>>();
+    });
+
+    it('should handle unknown', () => {
+        type T = {
+            _: unknown;
+        };
+        type Actual = Paths<T>;
+        type Expected = T;
+        assertType<Is<Actual, Expected>>();
+    });
+
+    it('should handle never', () => {
+        type T = {
+            _: never;
+        };
+        type Actual = Paths<T>;
+        type Expected = T;
+        assertType<Is<Actual, Expected>>();
+    });
+
+});
