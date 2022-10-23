54f284a6356c186ef9abf5494c03d1f924eab239
Daniel Dietrich <cafebab3@gmail.com>
2022-10-23 14:18:56 +0000

    Utility tests

--- src/utilities.ts
+++ src/utilities.ts
@@ -4,21 +4,50 @@
  * terms of the MIT License, which is available in the project root.
  ******************************************************************************/
 
-// ✅ [A, B] => A & B
+/** ✅
+ * Tuples to intersection
+ *
+ * [A, B] => A & B
+ *
+ * @param a tuple
+ * @returns an intersection
+ */
 export type TupleToIntersection<T extends any[]> =
     UnionToIntersection<TupleToUnion<T>>;
 
-// ✅ [A, B] => A | B
+/** ✅
+ * Tuple to union
+ *
+ * [A, B] => A | B
+ *
+ * @param a tuple
+ * @returns an union
+ */
 export type TupleToUnion<T extends any[]> =
     T[number];
 
-// ✅ A | B => A & B
+/** ✅
+ * Union to intersection
+ *
+ * A | B => A & B
+ *
+ * @param a union
+ * @returns an intersection
+ */
 export type UnionToIntersection<U> =
     (U extends any ? (arg: U) => void : never) extends ((arg: infer I) => void)
         ? I
         : never;
 
-// ✅ A | B => [A, B] or [B, A], the order is spooky 👻
+/** ✅
+ * Union to tuple. Use this with caution, the order of the tuple
+ * elements is not predictable.
+ *
+ * A | B => [A, B] or [B, A]
+ *
+ * @param a union
+ * @returns a tuple
+ */
 export type UnionToTuple<T> =
     [T] extends [never]
         ? []
