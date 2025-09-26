c7964c0bee3f37c1468be3010c9811f26a0acb14
Daniel Dietrich <cafebab3@gmail.com>
2025-09-26 20:05:33 +0200

    Updated copyright

--- src/type-checker.ts
+++ src/type-checker.ts
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright 2022 Daniel Dietrich
+ * Copyright 2025 Daniel Dietrich
  * This program and the accompanying materials are made available under the
  * terms of the MIT License, which is available in the project root.
  ******************************************************************************/
